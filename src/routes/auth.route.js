b5da7808b4edc36ee5d28e415392a5fa803d452b
Akib Jabed <a.jabed.bd@gmail.com>
2024-12-08 21:16:22 +0600

    Add swagger api documentation for auth route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -26,6 +26,7 @@ module.exports = router;
  *  post:
  *   summary: Register a new user
  *   description: Create a new user account with the provided details
+ *   tags: [Auth]
  *   requestBody:
  *    required: true
  *    content:
@@ -62,8 +63,8 @@ module.exports = router;
  *       value:
  *        name: Fake name
  *        email: fake@example.com
- *        password: "Password1"
- *        confirmPassword: "Password1"
+ *        password: Password1
+ *        confirmPassword: Password1
  *   responses:
  *    "201":
  *     description: User created successfully
@@ -80,6 +81,131 @@ module.exports = router;
  *     $ref: '#/components/responses/InvalidInput'
  *    "409":
  *     $ref: '#/components/responses/DuplicateEmail'
- *    "500":
- *     $ref: '#/components/responses/ServerError'
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *  post:
+ *   summary: User login
+ *   description: Authenticate user and provide further permission
+ *   tags: [Auth]
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       type: object
+ *       required:
+ *        - email
+ *        - password
+ *       properties:
+ *        email:
+ *         type: string
+ *         format: email
+ *         description: Email of the user
+ *        password:
+ *         type: string
+ *         format: password
+ *         description: Password of the user
+ *     examples:
+ *      validRequest:
+ *       value:
+ *        email: fake@example.com
+ *        password: Password1
+ *   responses:
+ *    "200":
+ *     description: User login successful
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        properties:
+ *         user:
+ *          $ref: '#/components/schemas/User'
+ *         token:
+ *          $ref: '#/components/schemas/Token'
+ *    "400":
+ *     $ref: '#/components/responses/InvalidInput'
+ *    "401":
+ *     description: Invalid credentials
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/Error'
+ *       example:
+ *        code: 401
+ *        message: Invalid credentials
+ */
+
+/**
+ * @swagger
+ * /auth/logout:
+ *  post:
+ *   summary: User logout
+ *   tags: [Auth]
+ *   security:
+ *    - bearerAuth: []
+ *   responses:
+ *    "204":
+ *     description: No content
+ *    "401":
+ *     $ref: '#/components/responses/Unauthorized'
+ *    "404":
+ *      $ref: '#/components/responses/NotFound'
+ */
+
+/**
+ * @swagger
+ * /auth/update-password:
+ *  post:
+ *   summary: User password update
+ *   tags: [Auth]
+ *   security:
+ *    - bearerAuth: []
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       type: object
+ *       required:
+ *        - currentPassword
+ *        - newPassword
+ *        - confirmPassword
+ *       properties:
+ *        currentPassword:
+ *         type: string
+ *         format: password
+ *         description: Current password of the user
+ *        newPassword:
+ *         type: string
+ *         format: password
+ *         description: New password of the user
+ *        confirmPassword:
+ *         type: string
+ *         format: password
+ *         description: Confirm new password of the user
+ *     examples:
+ *      validRequest:
+ *       value:
+ *        currentPassword: Password1
+ *        newPassword: Password01
+ *        confirmPassword: Password01
+ *   responses:
+ *    "200":
+ *     description: Password updated successfully
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        properties:
+ *         user:
+ *          $ref: '#/components/schemas/User'
+ *         token:
+ *          $ref: '#/components/schemas/Token'
+ *    "400":
+ *     $ref: '#/components/responses/InvalidInput'
+ *    "401":
+ *     $ref: '#/components/responses/Unauthorized'
  */
