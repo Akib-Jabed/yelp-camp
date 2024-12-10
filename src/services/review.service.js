d74cdf31f2578ed4466558509262a059b250f5c7
Akib Jabed <a.jabed.bd@gmail.com>
2024-12-10 18:53:25 +0600

    Update review service

--- src/services/review.service.js
+++ src/services/review.service.js
@@ -1,49 +1,17 @@
 const { Review, Campground } = require('../models');
 const ApiError = require('../utils/ApiError');
 
-const createReview = async (req) => {
+const checkCampground = async (req) => {
     const campgroundId = req.originalUrl.split('/')[3];
     const campground = await Campground.findById(campgroundId);
     if (!campground) {
         throw new ApiError(404, 'Campground not found');
     }
 
-    const review = new Review(req.body);
-    review.campground = campgroundId;
-    review.user = req.user.id;
-    await review.save();
-
-    return review;
+    return campgroundId;
 };
 
-const updateReview = async (req) => {
-    const campgroundId = req.originalUrl.split('/')[3];
-    const campground = await Campground.findById(campgroundId);
-    if (!campground) {
-        throw new ApiError(404, 'Campground not found');
-    }
-
-    const { reviewId } = req.params;
-    const review = await Review.findByIdAndUpdate(reviewId, { ...req.body });
-
-    if (!review) {
-        throw new ApiError(404, 'Review not found');
-    }
-    if (review.user.id !== req.user.id) {
-        throw new ApiError(403, 'Not authorized to update this review');
-    }
-
-    await review.save();
-    return review;
-};
-
-const deleteReview = async (req) => {
-    const campgroundId = req.originalUrl.split('/')[3];
-    const campground = await Campground.findById(campgroundId);
-    if (!campground) {
-        throw new ApiError(404, 'Campground not found');
-    }
-
+const checkReview = async (req) => {
     const { reviewId } = req.params;
     const review = await Review.findById(reviewId);
 
@@ -54,6 +22,30 @@ const deleteReview = async (req) => {
         throw new ApiError(403, 'Not authorized to delete this review');
     }
 
+    return reviewId;
+};
+
+const createReview = async (req) => {
+    const campgroundId = await checkCampground(req);
+
+    const review = new Review(req.body);
+    review.campground = campgroundId;
+    review.user = req.user.id;
+    await review.save();
+
+    return review;
+};
+
+const updateReview = async (req) => {
+    await checkCampground(req);
+    const reviewId = await checkReview(req);
+    const review = await Review.findByIdAndUpdate(reviewId, { ...req.body });
+    return review;
+};
+
+const deleteReview = async (req) => {
+    await checkCampground(req);
+    const reviewId = await checkReview(req);
     await Review.deleteOne({ _id: reviewId });
 };
 
