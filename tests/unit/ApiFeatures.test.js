99771a4748c341e1ba180283fb93e79f8c1e8241
Akib Jabed <a.jabed.bd@gmail.com>
2025-08-25 23:13:08 +0600

    Add unit test

--- tests/unit/ApiFeatures.test.js
+++ tests/unit/ApiFeatures.test.js
@@ -60,17 +60,73 @@ describe('ApiFeatures', () => {
             const apiFeatures = new ApiFeatures(mockQuery, queryString)
             const result = apiFeatures.sort()
 
-            expect()
+            expect(mockQuery.sort).toHaveBeenCalledWith("price -rating")
         })
 
         it('should use default sorting when no sorting field provided', () => {
+            const testQueryString = {
+                title: 'campground',
+                location: 'location'
+            }
+            const apiFeatures = new ApiFeatures(mockQuery, testQueryString)
+            const result = apiFeatures.sort()
 
+            expect(mockQuery.sort).toHaveBeenCalledWith("-createdAt")
         })
     })
 
     describe('paginate', () => {
         it('should paginate results with given page and limit', () => {
+            const apiFeatures = new ApiFeatures(mockQuery, queryString)
+            const result = apiFeatures.paginate()
 
+            const skipValue = (queryString.page - 1) * queryString.limit;
+            expect(mockQuery.skip).toHaveBeenCalledWith(skipValue);
+            expect(mockQuery.limit).toHaveBeenCalledWith(queryString.limit);
+        })
+
+        it('should paginate results with default page(1) if value not given', () => {
+            const testQueryString = {
+                title: 'campground',
+                location: 'location',
+                sort: 'price,-rating',
+                limit: 5 
+            }
+            const apiFeatures = new ApiFeatures(mockQuery, testQueryString)
+            const result = apiFeatures.paginate()
+
+            expect(mockQuery.skip).toHaveBeenCalledWith(0);
+            expect(mockQuery.limit).toHaveBeenCalledWith(testQueryString.limit);
+        })
+
+        it('should paginate results with default limit(10) if value not given', () => {
+            const testQueryString = {
+                title: 'campground',
+                location: 'location',
+                sort: 'price,-rating',
+                page: 2 
+            }
+            const apiFeatures = new ApiFeatures(mockQuery, testQueryString)
+            const result = apiFeatures.paginate()
+
+            const skipValue = (testQueryString.page - 1) * 10;
+            expect(mockQuery.skip).toHaveBeenCalledWith(skipValue);
+            expect(mockQuery.limit).toHaveBeenCalledWith(10);
+        })
+
+        
+
+        it('should paginate results with default page(1) and limit(10) if value not given', () => {
+            const testQueryString = {
+                title: 'campground',
+                location: 'location',
+                sort: 'price,-rating'
+            }
+            const apiFeatures = new ApiFeatures(mockQuery, testQueryString)
+            const result = apiFeatures.paginate()
+
+            expect(mockQuery.skip).toHaveBeenCalledWith(0);
+            expect(mockQuery.limit).toHaveBeenCalledWith(10);
         })
     })
 })
