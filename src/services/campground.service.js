1844cab48a15dbb4f7eea5185258c52c07430b94
Akib Jabed <a.jabed.bd@gmail.com>
2025-09-10 16:01:46 +0600

    Add test

--- src/services/campground.service.js
+++ src/services/campground.service.js
@@ -65,4 +65,5 @@ module.exports = {
     getCampgrounds,
     updateCampground,
     deleteCampground,
+    checkValidUser
 };
--- tests/services/campground-service.test.js
+++ tests/services/campground-service.test.js
@@ -1,4 +1,4 @@
-const { createCampground, getCampground, getCampgrounds, updateCampground, deleteCampground } = require('../../src/services/campground.service');
+const { createCampground, getCampground, getCampgrounds, updateCampground, deleteCampground, checkValidUser } = require('../../src/services/campground.service');
 const { Campground, Review } = require('../../src/models');
 const ApiError = require('../../src/utils/ApiError');
 const ApiFeatures = require('../../src/utils/ApiFeatures');
@@ -130,5 +130,67 @@ describe('campground service', () => {
             expect(mockApiFeatures.paginate).toHaveBeenCalled()
             expect(result).toBe(mockCampgrounds)
         })
+    })
+
+    describe('get campground', () => {
+        const mockReq = {
+            params: {
+                id: 1
+            }
+        }
+        const mockCampground = {
+            id: 1,
+            title: 'Campground Title',
+            description: 'Test campground description',
+            location: 'Test location',
+            price: 100,
+            images: ['image1.jpg', 'image2.png'],
+            user: 'user1',
+        }
+        beforeEach(() => {
+            Campground.findById = jest.fn()
+        })
+
+        it('should get campground by id', async () => {
+            Campground.findById.mockResolvedValue(mockCampground);
+            const result = await getCampground(mockReq)
+
+            expect(result).toBe(mockCampground)
+            expect(Campground.findById).toHaveBeenCalledWith(1)
+            expect(ApiError).not.toHaveBeenCalled()
+        })
+
+        it('should throw error if campground not found', async () => {
+            Campground.findById.mockResolvedValue(null);
+
+            await expect(getCampground(mockReq)).rejects.toThrow()
+            expect(ApiError).toHaveBeenCalledWith(404, 'Campground not found')
+        })
+    })
+
+    describe('check valid user', () => {
+        it('should throw error for different user', () => {
+            let campgroundUser = 1
+            let loggedUser = 2
+
+            expect(() => checkValidUser(campgroundUser, loggedUser)).toThrow();
+            expect(ApiError).toHaveBeenCalledWith(403, "Don't have access to take this action")
+        })
+
+        it('should not throw error for same user', () => {
+            let campgroundUser = 1
+            let loggedUser = 1
+
+            expect(() => checkValidUser(campgroundUser, loggedUser)).not.toThrow();
+            expect(ApiError).not.toHaveBeenCalled()
+        })
+    })
+
+    describe('update campground', () => {
+
+    })
+
+    describe('delete campground', () => {
+
     })
 })
